40fcc1bf2badc081ebc3321543e12797c200048d
Berenice Contreras <contbere@gmail.com>
2024-07-01 10:34:40 -0300

    Ejemplo de if

---
+++ js/02-condicionales.js
@@ -0,0 +1,16 @@
+// condicionales 
+
+/*
+if(CONDICIÓN LÓGICA){
+todas las líneas que quiero que sucedan si se cumple la condición lógica
+}
+*/
+
+//Solicitar al usuario ingresar un número y verificar si es positivo
+
+const numeroSolicitado = parseInt (prompt('Ingrese un número entero'));
+
+if(numeroSolicitado > 0){
+    document.write(`<p>El número ingresado: ${numeroSolicitado} es positivo`)
+    
+}
