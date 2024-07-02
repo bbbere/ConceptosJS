aec46b4028f887ed7780108a75805cf72fc7cc64
Berenice Contreras <contbere@gmail.com>
2024-07-01 22:51:12 -0300

    Completé lo último de la clase

--- js/02-condicionales.js
+++ js/02-condicionales.js
@@ -8,11 +8,24 @@ todas las líneas que quiero que sucedan si se cumple la condición lógica
 
 //Solicitar al usuario ingresar un número y verificar si es positivo
 
-const numeroSolicitado = parseInt (prompt('Ingrese un número entero'));
+// const numeroSolicitado = parseInt (prompt('Ingrese un número entero'));
 
-if(numeroSolicitado > 0){
-    document.write(`<p>El número ingresado: ${numeroSolicitado} es positivo`)
+// if(numeroSolicitado > 0){
+//     document.write(`<p>El número ingresado: ${numeroSolicitado} es positivo`)
 
-}else{
-    document.write(`<p> El número ingresado: ${numeroSolicitado} no es positivo `)
+// }else{
+//     document.write(`<p> El número ingresado: ${numeroSolicitado} no es positivo `)
+// }
+
+// Cree un programa que solicite al usuario su edad e indique si puede votar o no.
+// desde 18 años hasta 75 años es obligatorio
+// si la persona tiene 16 o 17 años es opcional, desde 75 años en adelante también es opcional.
+// menores de 16 no pueden votar 
+
+const edad = parseInt(prompt ('Ingrese su edad'));
+
+// AND &&
+// OR ||
+if(edad >= 18 && edad <= 75){
+document.write(`<p> Usted está obligado a votar</p>`);
 }
